51947018eb8c1a20e97aec53158826ee20b61f92
Mateus Andrade <Pagogosouza97@gmail.com>
2023-07-31 08:21:37 -0300

    Desenvolvimento rodape

--- assets/components/logo.js
+++ assets/components/logo.js
@@ -1,37 +1,24 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 
 export class Logo extends LitElement {
-    static styles = [
-        css`
-            :host {
-                width: 48px;
-                height: 48px;
-                
-                background-color:black;
-                -webkit-mask: url('../../Imagens/logo.svg');
-                mask: url('../../Imagens/logo.svg');
-                display: block;
-            }
-        `
-    ];
+  static styles = [
+    css`
+      :host {
+        width: 48px;
+        height: 48px;
 
-    render() {
-        return html`
-        <footer>
-        <nav>
-        <a href="">Contato</a>
-          <a href="">Portfolio</a>
-          <a href="services">Sobre</a>
-          <a href="baber">Teus Designer</a>
-          <a href="/">Home</a> 
-        </nav>
-        </footer>
-        <span>
-            <app-paragrafo></app-paragrafo>
-        </span>
-        
-        
-        `;
-    }
+        background-color: black;
+        -webkit-mask: url("../../Imagens/logo.svg");
+        mask: url("../../Imagens/logo.svg");
+        display: block;
+      }
+    `,
+  ];
+
+  render() {
+    return html`
+      
+    `;
+  }
 }
-customElements.define('app-logo', Logo);
+customElements.define("app-logo", Logo);
--- assets/components/rodape.js
+++ assets/components/rodape.js
@@ -11,11 +11,51 @@ export class Rodape extends LitElement {
         align-items: center;
         flex-shrink: 0;
       }
+
+      app-paragrafo {
+        color: #080808;
+        font-family: DM Mono;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        letter-spacing: -0.5px;
+      }
+
+      span {
+        display: flex;
+        width: 360px;
+        height: 31px;
+        padding: 9px 25px;
+        justify-content: center;
+        align-items: center;
+        gap: 10px;
+        position: absolute;
+        bottom: 0px;
+
+        background: rgba(0, 0, 0, 0.4);
+      }
     `,
   ];
 
   render() {
-    return html``;
+    return html`
+      <footer>
+        <nav>
+          <a href="">Contato</a>
+          <a href="">Portfolio</a>
+          <a href="services">Sobre</a>
+          <a href="baber">Teus Designer</a>
+          <a href="/">Home</a>
+        </nav>
+      </footer>
+      <span>
+        <app-paragrafo
+          >©Teus Desginer - Todos os direitos reservados desde
+          2023.</app-paragrafo
+        >
+      </span>
+    `;
   }
 }
 customElements.define("app-rodape", Rodape);
